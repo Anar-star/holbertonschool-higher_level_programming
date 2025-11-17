569fbce26d8a79d4b4184a3c7674875ac70c36f6
Anar-star <suvarovanar2@gmail.com>
2025-11-17 08:13:50 +0000

    a

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python3
 def print_last_digit(number):
-    """
-    Prints the last digit of a number and returns its value.
-    The last digit is always positive (0-9).
-    """
-    
-    # Calculate the last digit by taking the absolute value 
-    # of the number modulo 10.
     last_digit = abs(number) % 10
-    
-    # Print the digit without a newline
-    print(last_digit, end="")
-    
-    # Return the value of the last digit
+    print(last_digit, end="")  # end="" avoids adding a newline
     return last_digit
