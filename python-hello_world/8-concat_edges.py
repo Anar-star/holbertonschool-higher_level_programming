fd86ce323fc0c1977f7d1163c225a7127ccabbe3
Anar-star <suvarovanar2@gmail.com>
2025-11-17 06:02:52 +0000

    a

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[35:66] + str[66:71] + str[:6])
+ language that combines remarkable power with very clear syntax".split()
+print(str[5] + " " + str[6] + " " +  str[12] + " " + str[0])
