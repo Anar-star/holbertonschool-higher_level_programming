595c98599ce39258a102e7c2e8a1e810428fd3e8
Anar-star <suvarovanar2@gmail.com>
2025-11-30 10:08:28 +0000

    a

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization,
+attribute filtering, and the ability to reload from a dictionary.
+"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attribute names contained
+        in this list are included. Otherwise, all attributes are included.
+        """
+        if isinstance(attrs, list):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance.
+
+        json: dictionary where key is attribute name, value is attribute value
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
