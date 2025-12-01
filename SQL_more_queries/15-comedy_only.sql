9aa69d653bb628defc08905be127c1632cbcc379
Anar-star <suvarovanar2@gmail.com>
2025-12-01 13:17:32 +0000

    commited  a script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
---hbtn_0d_tvshows lists all the shows about Comedy genre to a database
+-- List all Comedy shows from the hbtn_0d_tvshows database
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
