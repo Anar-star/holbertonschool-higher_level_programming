be64314c73db7758e2dea9a1e713e160e1e6efcf
Anar-star <suvarovanar2@gmail.com>
2025-12-01 07:36:23 +0000

    a

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2 with password user_0d_2_pwd
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege only on hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
